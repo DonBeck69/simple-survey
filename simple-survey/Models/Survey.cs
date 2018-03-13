4393b34fbae684614d0f172ebbd909d1bd63d15f
Donovan Beck <donovan.beck.69@outlook.com>
2018-03-12 21:10:29 -0700

    add survey

---
+++ simple-survey/Models/Survey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobilityMaturityWebApp.Models
+{
+    public class Survey
+    {
+
+        public Survey() { }
+        public string SurveyName { get; set; }
+        public List<Question> Questions { get; set; }
+
+
+
+    }
+
+
+}
